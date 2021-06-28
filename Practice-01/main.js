69da368e12332e1cac13b2ce2e81b777a1366d2e
Karthik Balachandar <caarthik180@gmail.com>
2021-06-29 01:40:47 +0530

    Initial practice

---
+++ Practice-01/main.js
@@ -0,0 +1,30 @@
+
+var dog = {
+    name: 'Rover',
+    weight: 35
+}
+
+if (dog.weight>30) {
+    alert('WOOF WOOF')
+}
+else{
+    alert('woof woof')
+}
+
+
+
+setTimeout(() => {
+    alert('Are u serious?')
+}, 3000);
+
+
+setTimeout(wakeUpUser,5000)
+ 
+function wakeUpUser() {
+    alert('Are u OK?')
+}
+
+
+ setTimeout(() => {
+     
+ }, timeout);
