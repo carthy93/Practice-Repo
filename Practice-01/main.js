56b3bff568cbe67a339077782ac3debb5af0db7f
Karthik Balachandar <caarthik180@gmail.com>
2021-06-29 19:14:39 +0530

    Difference between var,let &const

--- Practice-01/main.js
+++ Practice-01/main.js
@@ -25,6 +25,28 @@ function wakeUpUser() {
 }
 
 
- setTimeout(() => {
-     
- }, timeout);
+
+
+
+ // Difference between var and let 
+
+ function start() {
+    //suppose if we declare var i = 0 in for loop,  we will not get the error for second console.log
+     for (let i=0;i<5;i++) {
+         console.log(i)
+     }
+
+     console.log(i)  // here var is function scoped , let and const are  block scoped
+ }
+
+ start()
+
+
+ //var variable can be declared, (overwritten), (re-assigned) many time , which sometimes leads to problem in a program
+
+ //in ES6 (ES2015) let and const were introduced
+
+ //let and const have same properties, but const cannot be re-assigned as the name itself implies "Constant"
+
+ //var creates a property on the global window object 
+ 
